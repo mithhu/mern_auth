fd45ca149a170446d10fcf56d5bcc0880867cb56
mithhu <mithhu666@gmail.com>
2023-10-31 00:18:30 +0800

    added frontend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import connectDB from './config/db.js';
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5001;
-import userRoutes from './routes/userRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
 
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -16,13 +17,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/api/users", userRoutes);
 
-app.use('/api/users', userRoutes);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('/', (req, res) => res.send('API running'));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
